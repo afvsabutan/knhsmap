ef16623aaf31ff8e77fc1f629eef9a99ae0eda78
afvsabutan <afvsabutan@gmail.com>
2026-01-22 12:42:11 +0800

    Add files via upload

---
+++ config/maps.php
@@ -0,0 +1,544 @@
+<?php
+
+return array (
+  'map1' => 
+  array (
+    'forward' => 'map40',
+    'back' => NULL,
+    'left' => 'map3',
+    'right' => 'map2',
+    '' => NULL,
+  ),
+  'map2' => 
+  array (
+    'forward' => 'map5',
+    'back' => NULL,
+    'left' => 'map1',
+    'right' => 'map4',
+  ),
+  'map3' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map11',
+    'left' => 'map4',
+    'right' => 'map1',
+  ),
+  'map4' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map2',
+    'right' => 'map3',
+  ),
+  'map5' => 
+  array (
+    'forward' => 'map8',
+    'back' => 'map2',
+    'left' => 'map7',
+    'right' => 'map6',
+  ),
+  'map6' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map5',
+    'right' => NULL,
+  ),
+  'map7' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => NULL,
+    'right' => 'map5',
+  ),
+  'map8' => 
+  array (
+    'forward' => 'map9',
+    'back' => 'map5',
+    'left' => 'map10',
+    'right' => 'map12',
+  ),
+  'map9' => 
+  array (
+    'forward' => 'map16',
+    'back' => 'map8',
+    'left' => 'map14',
+    'right' => 'map13',
+  ),
+  'map10' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map11',
+    'right' => 'map8',
+  ),
+  'map11' => 
+  array (
+    'forward' => 'map3',
+    'back' => 'map15',
+    'left' => 'map12',
+    'right' => 'map10',
+  ),
+  'map12' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map8',
+    'right' => 'map11',
+  ),
+  'map13' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map9',
+    'right' => 'map15',
+  ),
+  'map14' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map15',
+    'right' => 'map9',
+  ),
+  'map15' => 
+  array (
+    'forward' => 'map11',
+    'back' => 'map18',
+    'left' => 'map13',
+    'right' => 'map14',
+  ),
+  'map16' => 
+  array (
+    'forward' => 'map20',
+    'back' => 'map9',
+    'left' => 'map17',
+    'right' => 'map19',
+  ),
+  'map17' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map18',
+    'right' => 'map16',
+  ),
+  'map18' => 
+  array (
+    'forward' => 'map15',
+    'back' => 'map23',
+    'left' => 'map19',
+    'right' => 'map17',
+  ),
+  'map19' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map16',
+    'right' => 'map18',
+  ),
+  'map20' => 
+  array (
+    'forward' => 'map24',
+    'back' => 'map16',
+    'left' => 'map21',
+    'right' => 'map22',
+  ),
+  'map21' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map23',
+    'right' => 'map20',
+  ),
+  'map22' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map20',
+    'right' => 'map23',
+  ),
+  'map23' => 
+  array (
+    'forward' => 'map18',
+    'back' => 'map27',
+    'left' => 'map22',
+    'right' => 'map21',
+  ),
+  'map24' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map20',
+    'left' => 'map26',
+    'right' => 'map25',
+  ),
+  'map25' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map31',
+    'left' => 'map24',
+    'right' => 'map27',
+  ),
+  'map26' => 
+  array (
+    'forward' => 'map28',
+    'back' => NULL,
+    'left' => 'map27',
+    'right' => 'map24',
+  ),
+  'map27' => 
+  array (
+    'forward' => 'map23',
+    'back' => NULL,
+    'left' => 'map25',
+    'right' => 'map26',
+  ),
+  'map28' => 
+  array (
+    'forward' => 'map32',
+    'back' => 'map26',
+    'left' => 'map30',
+    'right' => 'map29',
+  ),
+  'map29' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map28',
+    'right' => 'map31',
+  ),
+  'map30' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map31',
+    'right' => 'map28',
+  ),
+  'map31' => 
+  array (
+    'forward' => 'map25',
+    'back' => 'map37',
+    'left' => 'map29',
+    'right' => 'map30',
+  ),
+  'map32' => 
+  array (
+    'forward' => 'map35',
+    'back' => 'map28',
+    'left' => 'map34',
+    'right' => 'map33',
+  ),
+  'map33' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map32',
+    'right' => NULL,
+  ),
+  'map34' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => NULL,
+    'right' => 'map32',
+  ),
+  'map35' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map32',
+    'left' => 'map36',
+    'right' => 'map38',
+  ),
+  'map36' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map37',
+    'right' => 'map35',
+  ),
+  'map37' => 
+  array (
+    'forward' => 'map31',
+    'back' => NULL,
+    'left' => 'map38',
+    'right' => 'map36',
+  ),
+  'map38' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map35',
+    'right' => 'map37',
+  ),
+  'map39' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map42',
+    'right' => 'map40',
+  ),
+  'map40' => 
+  array (
+    'forward' => 'map43',
+    'back' => 'map1',
+    'left' => 'map39',
+    'right' => 'map41',
+  ),
+  'map41' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map40',
+    'right' => 'map42',
+  ),
+  'map42' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map49',
+    'left' => 'map41',
+    'right' => 'map39',
+  ),
+  'map43' => 
+  array (
+    'forward' => 'map47',
+    'back' => 'map40',
+    'left' => 'map45',
+    'right' => 'map44',
+  ),
+  'map44' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map43',
+    'right' => NULL,
+  ),
+  'map45' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => NULL,
+    'right' => 'map43',
+  ),
+  'map46' => 
+  array (
+    'forward' => 'map50',
+    'back' => NULL,
+    'left' => 'map47',
+    'right' => 'map49',
+  ),
+  'map47' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map43',
+    'left' => 'map48',
+    'right' => 'map46',
+  ),
+  'map48' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map52',
+    'left' => 'map49',
+    'right' => 'map47',
+  ),
+  'map49' => 
+  array (
+    'forward' => 'map42',
+    'back' => NULL,
+    'left' => 'map46',
+    'right' => 'map48',
+  ),
+  'map50' => 
+  array (
+    'forward' => 'map54',
+    'back' => 'map46',
+    'left' => 'map51',
+    'right' => 'map53',
+  ),
+  'map51' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map52',
+    'right' => 'map50',
+  ),
+  'map52' => 
+  array (
+    'forward' => 'map48',
+    'back' => 'map57',
+    'left' => 'map53',
+    'right' => 'map51',
+  ),
+  'map53' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map50',
+    'right' => 'map52',
+  ),
+  'map54' => 
+  array (
+    'forward' => 'map58',
+    'back' => 'map50',
+    'left' => 'map55',
+    'right' => 'map56',
+  ),
+  'map55' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map57',
+    'right' => 'map54',
+  ),
+  'map56' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map54',
+    'right' => 'map57',
+  ),
+  'map57' => 
+  array (
+    'forward' => 'map52',
+    'back' => 'map61',
+    'left' => 'map56',
+    'right' => 'map55',
+  ),
+  'map58' => 
+  array (
+    'forward' => 'map62',
+    'back' => 'map54',
+    'left' => 'map59',
+    'right' => 'map60',
+  ),
+  'map59' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map61',
+    'right' => 'map58',
+  ),
+  'map60' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map58',
+    'right' => 'map61',
+  ),
+  'map61' => 
+  array (
+    'forward' => 'map57',
+    'back' => 'map65',
+    'left' => 'map60',
+    'right' => 'map59',
+  ),
+  'map62' => 
+  array (
+    'forward' => 'map66',
+    'back' => 'map58',
+    'left' => 'map63',
+    'right' => 'map64',
+  ),
+  'map63' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map65',
+    'right' => 'map62',
+  ),
+  'map64' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map62',
+    'right' => 'map65',
+  ),
+  'map65' => 
+  array (
+    'forward' => 'map61',
+    'back' => 'map69',
+    'left' => 'map64',
+    'right' => 'map63',
+  ),
+  'map66' => 
+  array (
+    'forward' => 'map70',
+    'back' => 'map62',
+    'left' => 'map67',
+    'right' => 'map68',
+  ),
+  'map67' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map69',
+    'right' => 'map66',
+  ),
+  'map68' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map66',
+    'right' => 'map69',
+  ),
+  'map69' => 
+  array (
+    'forward' => 'map65',
+    'back' => 'map71',
+    'left' => 'map68',
+    'right' => 'map67',
+  ),
+  'map70' => 
+  array (
+    'forward' => 'map74',
+    'back' => 'map66',
+    'left' => 'map73',
+    'right' => 'map72',
+  ),
+  'map72' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map70',
+    'right' => 'map71',
+  ),
+  'map73' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map71',
+    'right' => 'map70',
+  ),
+  'map71' => 
+  array (
+    'forward' => 'map69',
+    'back' => 'map77',
+    'left' => 'map72',
+    'right' => 'map73',
+  ),
+  'map74' => 
+  array (
+    'forward' => NULL,
+    'back' => 'map70',
+    'left' => 'map75',
+    'right' => 'map76',
+  ),
+  'map75' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map77',
+    'right' => 'map74',
+  ),
+  'map76' => 
+  array (
+    'forward' => NULL,
+    'back' => NULL,
+    'left' => 'map74',
+    'right' => 'map77',
+  ),
+  'map77' => 
+  array (
+    'forward' => 'map71',
+    'back' => NULL,
+    'left' => 'map76',
+    'right' => 'map75',
+  ),
+);
